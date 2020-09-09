c9eae41655bce69b9433971f1376c65f4a7c2b9b
hf73 <hf73@njit.edu>
2020-09-09 14:23:04 -0400

    add hw1

---
+++ HW1/Homework.java
@@ -0,0 +1,14 @@
+public class Homework{
+   public static void main(String[] args) {
+      
+      String name = "Hany";
+      System.out.println(name);
+
+      int age = 19;
+      System.out.println(age);
+      
+      double decimal = 3.98;
+      System.out.println(decimal); 
+      
+}
+}
